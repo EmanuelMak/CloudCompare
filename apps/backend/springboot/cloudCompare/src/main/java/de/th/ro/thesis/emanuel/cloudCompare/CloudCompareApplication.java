d5b50471fc4d3ac35101ca9703ee3d42ca171524
sINFemmakk <emanuel.mak@stud.th-rosenheim.de>
2023-08-14 13:13:13 +0200

    init springboot and nest js

---
+++ apps/backend/springboot/cloudCompare/src/main/java/de/th/ro/thesis/emanuel/cloudCompare/CloudCompareApplication.java
@@ -0,0 +1,13 @@
+package de.th.ro.thesis.emanuel.cloudCompare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudCompareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudCompareApplication.class, args);
+	}
+
+}
---
+++ apps/backend/springboot/cloudCompare/src/test/java/de/th/ro/thesis/emanuel/cloudCompare/CloudCompareApplicationTests.java
@@ -0,0 +1,13 @@
+package de.th.ro.thesis.emanuel.cloudCompare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudCompareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
